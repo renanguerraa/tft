7adc192b72145a9f723ab198de901880b6d0eb65
Renan Guerra <re.galanguerra@gmail.com>
2019-08-24 17:54:42 -0300

    Added part of item category

--- TFTLOL.java
+++ TFTLOL.java
@@ -1,5 +1,3 @@
-package tft.lol;
-
 import java.util.Scanner;
 public class TFTLOL {
     
@@ -9,130 +7,245 @@ public class TFTLOL {
         
         int x, y, z;
         
-        System.out.println("Deseja saber sobre: \n 1- Campe玫es"
-                + " \n 2- Itens");
-        x = teclado.nextInt();
-        
-        if(x == 1){
+        do{ 
             
-            do{
+            System.out.println("----------");
+            System.out.println("Deseja saber sobre: \n 1- Campeoes"
+                    + " \n 2- Itens");
+            x = teclado.nextInt();
 
+            if(x == 1){
+
+                    System.out.println("----------");
+                    System.out.println("Deseja saber sobre: \n 1- Assassino "
+                            + "\n 2- Mestre das Laminas"
+                            + "\n 3- Lutador"
+                            + "\n 4- Elementalista"
+                            + "\n 5- Guardiao"
+                            + "\n 6- Pistoleiro"
+                            + "\n 7- Cavaleiro"
+                            + "\n 8- Patrulheiro"
+                            + "\n 9- Metamorfo"
+                            + "\n 10- Feiticeiro");
+                    y = teclado.nextInt();
+
+                    if(y == 1){
+
+                        System.out.println("----------");
+                        System.out.println(" Demonio: Evelynn(3)"
+                                + "\n Imperial: Katarina(3)"
+                                + "\n Ninja: Zed(2), Akali(4)"
+                                + "\n Pirata: Pyke(2)"
+                                + "\n Selvagem: Rengar(3)"
+                                + "\n Vastinata: Kha'Zix(1)");
+                    }
+
+                    if(y == 2){
+
+                        System.out.println("----------");
+                        System.out.println(" Demonio: Aatrox(3)"
+                                + "\n Exilado: Yasuo(5)"
+                                + "\n Imperial: Draven(4)"
+                                + "\n Nobre: Fiora(1)"
+                                + "\n Ninja: Shen(2)"
+                                + "\n Pirata: Gangplank(3)"
+                                + "\n Hextec: Camille(1)");
+                    }
+
+                    if(y == 3){
+
+                        System.out.println("----------");
+                        System.out.println(" Glacial: Volibear(3)"
+                                + "\n Robo: Blitzcrank(2)"
+                                + "\n Selvagem: Warwick(1)"
+                                + "\n Vastinata: Rek'Sai(2), Cho'Gath(4)"
+                                + "\n Hextec: Vi(3)");
+                    }
+
+                    if(y == 4){
+
+                        System.out.println("----------");
+                        System.out.println(" Demonio: Brand(4)"
+                                + "\n Glacial: Lissandra(2), Anivia(5)"
+                                + "\n Ninja: Kennen(3)"
+                                + "\n Yordle: Kennen(3)");
+                    }
+
+                    if(y == 5){
+                       System.out.println("----------");
+                       System.out.println(" Glacial: Braum(2)"
+                               + "\n Nobre: Leona(4)");
+                    }
+
+                    if(y == 6){
+                        System.out.println("----------");
+                        System.out.println(" Nobre: Lucian(2)"
+                                + "\n Pirata: Graves(1), Gangplank(3), Miss Fortune(5)"
+                                + "\n Yordle: Tristana(1)"
+                                + "\n Hextec: Jinx(4)");
+                    }
+
+                    if(y == 7){
+                        System.out.println("----------");
+                        System.out.println(" Glacial: Sejuani(4)"
+                                + "\n Imperial: Darius(1)"
+                                + "\n Nobre: Garen(1), Kayle(5)"
+                                + "\n Fantasma: Morderkaiser(1)"
+                                + "\n Yordle: Poppy(3)");
+                    }
+
+                    if(y == 8){
+                        System.out.println("----------");
+                        System.out.println(" Demonio: Varus(2)"
+                                + "\n Glacial: Ashe(3)"
+                                + "\n Nobre: Vayne(1)"
+                                + "\n Fantasma: Kindred(4)");
+                    }
+
+                    if(y == 9){
+                        System.out.println("----------");
+                        System.out.println(" Demonio: Elise(1), Swain(5)"
+                                + "\n Imperial: Swain(5)"
+                                + "\n Dragao: Shyvana(3)"
+                                + "\n Selvagem: Nidalee(1), Gnar(4)"
+                                + "\n Yordle: Gnar(4)"
+                                + "\n Hextec: Jayce(2)");
+                    }
+
+                    if(y == 10){
+                        System.out.println("----------");
+                        System.out.println(" Demonio: Morgana(3)"
+                                + "\n Dragao: Aurelion(4)"
+                                + "\n Fantasma: Karthus(5)"
+                                + "\n Selvagem: Ahri(2)"
+                                + "\n Vastinata: Kassadin(1)"
+                                + "\n Yordle: Lulu(2), Veigar(3)");
+                    }
+                }
+
+            if(x == 2){
+                
                 System.out.println("----------");
-                System.out.println("Deseja saber sobre: \n 1- Assassino "
-                        + "\n 2- Mestre das L芒minas"
-                        + "\n 3- Lutador"
-                        + "\n 4- Elementalista"
-                        + "\n 5- Guardi茫o"
-                        + "\n 6- Pistoleiro"
-                        + "\n 7- Cavaleiro"
-                        + "\n 8- Patrulheiro"
-                        + "\n 9- Metamorfo"
-                        + "\n 10- Feiticeiro");
+                System.out.println("Deseja saber sobre: \n 1- Espada G. p. C.(+AD)"
+                        + "\n 2- Arco Recurvo (+AS)"
+                        + "\n 3- Bastao Desnecessariamente Grande (+Dano em Habilidades)"
+                        + "\n 4- Lagrima da Deusa (+Mana)"
+                        + "\n 5- Cota de Malha (+Armor)"
+                        + "\n 6- Cinto do Gigante (+HP)"
+                        + "\n 7- Capa Negatron (+MR)"
+                        + "\n 8- Espatula (Faz Coisas)");
                 y = teclado.nextInt();
-
+                
                 if(y == 1){
-
+                    
                     System.out.println("----------");
-                    System.out.println("Dem么nio: Evelynn(3)"
-                            + "\n Imperial: Katarina(3)"
-                            + "\n Ninja: Zed(2), Akali(4)"
-                            + "\n Pirata: Pyke(2)"
-                            + "\n Selvagem: Rengar(3)"
-                            + "\n Vastinata: Kha'Zix(1)");
+                    System.out.println(" + Espada G. p. C. = Gume do Infinito (Mais dano em criticos)"
+                            + "\n + Arco Recurvo = Lamina do Divino (Ganha chance de crit. por segundo)"
+                            + "\n + Bastao Desnecessariamente Grande = Pistola Laminar Hextec (Cura em todo dano causado)"
+                            + "\n + Lagrima da Deusa = Lan鏰 de Shojin (Ganha % da mana max por atq apos cast)"
+                            + "\n + Cota de Malha = Anjo Guardiao (Revive com HP fixa)"
+                            + "\n + Cinto do Gigante = Arauto de Zeke (Aliados prox. ganham AS)"
+                            + "\n + Capa Negatron = Sedenta por Sangue (AA curam em % do dano)"
+                            + "\n + Espatula = Lamina Fantasma de Youmuu (Un. tambem e um assassino)");
                 }
-
+                
                 if(y == 2){
-
+                    
                     System.out.println("----------");
-                    System.out.println("Dem么nio: Aatrox(3)"
-                            + "\n Exilado: Yasuo(5)"
-                            + "\n Imperial: Draven(4)"
-                            + "\n Nobre: Fiora(1)"
-                            + "\n Ninja: Shen(2)"
-                            + "\n Pirata: Gangplank(3)"
-                            + "\n Hextec: Camille(1)");
+                    System.out.println(" + Espada G. p. C. = Lamina do Divino ()"
+                            + "\n + Arco Recurvo = Canhao Fumegante ()"
+                            + "\n + Bastao Desnecessariamente Grande = Lamina da Furia de Guinsoo ()"
+                            + "\n + Lagrima da Deusa = Faca de Statikk ()"
+                            + "\n + Cota de Malha = Dan鏰rina Fantasma ()"
+                            + "\n + Cinto do Gigante = Hidra Tit鈔ica ()"
+                            + "\n + Capa Negatron = Lamina Amaldi鏾ada ()"
+                            + "\n + Espatula = Espada do Rei Destruido ()");
                 }
-
+                
                 if(y == 3){
                     
                     System.out.println("----------");
-                    System.out.println("\n Glacial: Volibear(3)"
-                            + "\n Rob么: Blitzcrank(2)"
-                            + "\n Selvagem: Warwick(1)"
-                            + "\n Vastinata: Rek'Sai(2), Cho'Gath(4)"
-                            + "\n Hextec: Vi(3)");
+                    System.out.println(" + Espada G. p. C. = Pistola Laminar Hextec ()"
+                            + "\n + Arco Recurvo = Lamina da Furia de Guinsoo ()"
+                            + "\n + Bastao Desnecessariamente Grande = Capuz da Morte de Rabadon ()"
+                            + "\n + Lagrima da Deusa = Eco de Luden ()"
+                            + "\n + Cota de Malha = Medalhao dos Solari de Ferro ()"
+                            + "\n + Cinto do Gigante = Morellonomicon ()"
+                            + "\n + Capa Negatron = Centelha Ionica ()"
+                            + "\n + Espatula = Yuumi ()");
                 }
-
+                
                 if(y == 4){
                     
                     System.out.println("----------");
-                    System.out.println("\n Dem么nio: Brand(4)"
-                            + "\n Glacial: Lissandra(2), Anivia(5)"
-                            + "\n Ninja: Kennen(3)"
-                            + "\n Yordle: Kennen(3)");
-                }
-
-                if(y == 5){
-                   System.out.println("----------");
-                   System.out.println("\n Glacial: Braum(2)"
-                           + "\n Nobre: Leona(4)");
-                }
-
-                if(y == 6){
-                    System.out.println("----------");
-                    System.out.println("\n Nobre: Lucian(2)"
-                            + "\n Pirata: Graves(1), Gangplank(3), Miss Fortune(5)"
-                            + "\n Yordle: Tristana(1)"
-                            + "\n Hextec: Jinx(4)");
-                }
-
-                if(y == 7){
-                    System.out.println("----------");
-                    System.out.println("\n Glacial: Sejuani(4)"
-                            + "\n Imperial: Darius(1)"
-                            + "\n Nobre: Garen(1), Kayle(5)"
-                            + "\n Fantasma: Morderkaiser(1)"
-                            + "\n Yordle: Poppy(3)");
-                }
-
-                if(y == 8){
-                    System.out.println("----------");
-                    System.out.println("\n Dem么nio: Varus(2)"
-                            + "\n Glacial: Ashe(3)"
-                            + "\n Nobre: Vayne(1)"
-                            + "\n Fantasma: Kindred(4)");
-                }
-
-                if(y == 9){
-                    System.out.println("----------");
-                    System.out.println("\n Dem么nio: Elise(1), Swain(5)"
-                            + "\n Imperial: Swain(5)"
-                            + "\n Drag茫o: Shyvana(3)"
-                            + "\n Selvagem: Nidalee(1), Gnar(4)"
-                            + "\n Yordle: Gnar(4)"
-                            + "\n Hextec: Jayce(2)");
-                }
-
-                if(y == 10){
-                    System.out.println("----------");
-                    System.out.println("\n Dem么nio: Morgana(3)"
-                            + "\n Drag茫o: Aurelion(4)"
-                            + "\n Fantasma: Karthus(5)"
-                            + "\n Selvagem: Ahri(2)"
-                            + "\n Vastinata: Kassadin(1)"
-                            + "\n Yordle: Lulu(2), Veigar(3)");
+                    System.out.println(" + Espada G. p. C. = Lan鏰 de Shojin ()"
+                            + "\n + Arco Recurvo = Faca de Statikk ()"
+                            + "\n + Bastao Desnecessariamente Grande = Eco de Luden ()"
+                            + "\n + Lagrima da Deusa = Abra鏾 de Seraph ()"
+                            + "\n + Cota de Malha = Cora鏰o Congelado ()"
+                            + "\n + Cinto do Gigante = Reden鏰o ()"
+                            + "\n + Capa Negatron = Quietude"
+                            + "\n + Espatula = Darkin");
                 }
                 
-                System.out.println("----------"
-                        + "\n Deseja saber algo mais? \n 1- Sim"
-                        + "\n 2- Nao");
-                z = teclado.nextInt();
+                if(y == 5){
+                    
+                    System.out.println("----------");
+                    System.out.println(" + Espada G. p. C. = Anjo Guardiao ()"
+                            + "\n + Arco Recurvo = Dan鏰rina Fantasma ()"
+                            + "\n + Bastao Desnecessariamente Grande = Medalhao dos Solari de Ferro ()"
+                            + "\n + Lagrima da Deusa = Cora鏰o Congelado ()"
+                            + "\n + Cota de Malha = Armadura de Espinhos ()"
+                            + "\n + Cinto do Gigante = Buff Vermelho ()"
+                            + "\n + Capa Negatron = Quebra-lamina ()"
+                            + "\n + Espatula = Juramento do Cavaleiro ()");
+                }
+                
+                if(y == 6){
+                    
+                    System.out.println("----------");
+                    System.out.println(" + Espada G. p. C. = Heraldo de Zeke ()"
+                            + "\n + Arco Recurvo = Hidra Titanica ()"
+                            + "\n + Bastao Desnecessariamente Grande = Morellonomicon ()"
+                            + "\n + Lagrima da Deusa = Reden鏰o ()"
+                            + "\n + Cota de Malha = Buff Vermelho ()"
+                            + "\n + Cinto do Gigante = Armadura de Warmog ()"
+                            + "\n + Capa Negatron = Zefiro ()"
+                            + "\n + Espatula = Malho Congelado ()");
+                }
+                
+                if(y == 7){
+                    
+                    System.out.println("----------");
+                    System.out.println(" + Espada G. p. C. = Sedenta por Sangue ()"
+                            + "\n + Arco Recurvo = Lamina Amaldi鏾ada ()"
+                            + "\n + Bastao Desnecessariamente Grande = Centelha Ionica ()"
+                            + "\n + Lagrima da Deusa = Quietude ()"
+                            + "\n + Cota de Malha = Quebra-lamina ()"
+                            + "\n + Cinto do Gigante = Zefiro ()"
+                            + "\n + Capa Negatron = Garra de Dragao ()"
+                            + "\n + Espatula = Furacao de Runaan ()");
+                }
+                
+                if(y == 8){
+                    
+                    System.out.println("----------");
+                    System.out.println(" + Espada G. p. C. = Lamina Fantasma de Youmuu ()"
+                            + "\n + Arco Recurvo = Espada do Rei Destru韉o ()"
+                            + "\n + Bastao Desnecessariamente Grande = Yuumi ()"
+                            + "\n + Lagrima da Deusa = Darkin ()"
+                            + "\n + Cota de Malha = Juramento do Cavaleiro ()"
+                            + "\n + Cinto do Gigante = Malho Congelado ()"
+                            + "\n + Capa Negatron = Furacao de Runaan ()"
+                            + "\n + Espatula = For鏰 da Natureza ()");
+                }
             }
-            while(z == 1);
+            
+            System.out.println("----------");
+            System.out.println("Deseja saber mais alguma coisa? \n 1- Sim"
+                        + "\n 2- Nao");
+            z = teclado.nextInt(); 
         }
-        
-        if(x == 2){
-            System.out.println("jaja eu fa莽o");
-        }
+        while(z == 1);
     }
 }
